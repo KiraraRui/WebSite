a2ef47326348622ac78f9431772385f04e5ad32b
zbcanols21 <zbcanols21@zbc.dk>
2020-09-10 15:41:47 +0200

    funckasd

--- Doctor/src/app/front-page/front-page.component.ts
+++ Doctor/src/app/front-page/front-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {ReadLocalFileService} from '../read-local-file.service';
 
 @Component({
   selector: 'app-front-page',
@@ -7,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontPageComponent implements OnInit {
 
-  frontPageSlideOne = {"Pic" : "../../assets/FrontPage/SlideOne.png", "Des" : "../../assets/FrontPage/SlideOneDescription"};
-  frontPageSlideTwo = {"Pic" : "../../assets/FrontPage/SlideTwo.jpg", "Des" : "../../assets/FrontPage/SlideTwoDescription.png"};
+  //fileReaderFront =  this.getTextFile();
+  frontPageSlideOne = {"Pic" : "../../assets/FrontPage/SlideOne.png", "Des" : "../../assets/FrontPage/SlideTwoDescription.png"};
+  frontPageSlideTwo = {"Pic" : "../../assets/FrontPage/SlideTwo.jpg", "Des" : "../../assets/FrontPage/SlideTwoDescription.jpg"};
   frontPageSlideTree = {"Pic" : "../../assets/FrontPage/SlideTree.png", "Des" : "../../assets/FrontPage/SlideTreeDescription.png"};
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    console.debug(this.getTextFile());
   }
+  getTextFile(){
+    console.debug("ARgh!!");
+    return this.http.get(this.frontPageSlideOne.Des, {responseType: 'text'});
 
+  }
 }
